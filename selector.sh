b8d57d02b33eebddadaa1abde2ff450f17665c64
BrilloCloud <130512571+BrilloCloud@users.noreply.github.com>
2023-05-15 18:16:45 +0300

    Update selector.sh

--- selector.sh
+++ selector.sh
@@ -5,5 +5,5 @@ if [ "${ARCH}" == "arm64" ];
 then
 bash <(curl -s https://raw.githubusercontent.com/BrilloCloud/VPS-Normal/main/ARM64-installer.sh)
 else 
-bash <(curl -s https://raw.githubusercontent.com/Ashu11-A/Ashu_eggs/main/vps/64x/installer.sh)
+bash <(curl -s https://raw.githubusercontent.com/BrilloCloud/VPS-Normal/main/x64-installer.sh)
 fi
